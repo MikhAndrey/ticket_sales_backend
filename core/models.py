bd33eb2064ca3122d93f2c286a860fc6976c59d6
MikhAndrey <mikhalevange@gmail.com>
2024-05-19 12:43:02 +0300

    Updated is_staff field default value

--- core/models.py
+++ core/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser):
     last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     requested_role = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
 
