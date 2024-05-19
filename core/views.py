8b3c3aac723d2a1c50e4b822f7e90c731454c1fe
MikhAndrey <mikhalevange@gmail.com>
2024-05-19 16:16:56 +0300

    Added cities management

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+from core.models import City
+from core.serializers import CitySerializer
+
+
+class CityListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        cities = City.objects.all()
+        serializer = CitySerializer(cities, many=True)
+        return HttpResponse(serializer.data, status=200)
